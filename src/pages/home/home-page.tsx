37606a62466211f064ff42968e5f23237213d1f5
eeennsu <diso592@naver.com>
2025-10-10 17:49:54 +0900

    fix: deploy test

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 
 export default function HomePage() {
   return (
-    <div className='bg-amber-800 text-7xl text-blue-600'>NEW ECS LIVE TEST!</div>
+    <div className='bg-green-500 text-7xl text-amber-200'>EUNSU POWER V3</div>
     // <div className='min-h-screen bg-gradient-to-b from-zinc-950 via-zinc-900 to-black text-zinc-100'>
     //   {/* Nav */}
     //   <header className='sticky top-0 z-20 border-b border-zinc-800/60 backdrop-blur supports-[backdrop-filter]:bg-zinc-950/60'>
