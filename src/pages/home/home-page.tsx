acd554583e44c658cec27aab0c798973b148dab7
eeennsu <diso592@naver.com>
2025-10-11 14:36:50 +0900

    fix: deploy test

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 
 export default function HomePage() {
   return (
-    <div className='text-pink-600-200 bg-blue-500 text-7xl'>HELLO EUNSU</div>
+    <div className='text-pink-600-200 bg-pink-500 text-7xl'>HELLO EUNSU222222</div>
     // <div className='min-h-screen bg-gradient-to-b from-zinc-950 via-zinc-900 to-black text-zinc-100'>
     //   {/* Nav */}
     //   <header className='sticky top-0 z-20 border-b border-zinc-800/60 backdrop-blur supports-[backdrop-filter]:bg-zinc-950/60'>
